83a739a5566cfd4693b3d7452f41436bbd52dd86
Benjamin DuPont <ben@nebland.com>
2015-06-08 16:17:57 -0500

    remove dead code

--- test1/main.cpp
+++ test1/main.cpp
@@ -13,18 +13,11 @@
 int main()
 {
 	asio::io_service i;
-	// asio::deadline_timer t(i);
-	// asio::basic_waitable_timer::system_timer t(i);
 	asio::system_timer t(i);
 
 	t.expires_from_now(std::chrono::milliseconds(5000));
-	// t.expires_from_now(std::chrono::seconds(5));
 
 	t.wait();
-
-	//std::chrono::time_point<std::chrono::system_clock> start, end;
-
-	//end = std::chrono::system_clock::now();
 
 	asio::error_code ec;
 
