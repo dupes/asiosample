b8dedae154c156ad97a158ddd0ff5242caa23c30
Benjamin DuPont <ben@nebland.com>
2015-06-08 18:20:45 -0500

    use std::bind

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,29 +1,57 @@
 
 #include <iostream>
 
+#include <functional>
 #include <chrono>
 
 #include "asio.hpp"
 
 #include "asio/system_timer.hpp"
+#include "asio/steady_timer.hpp"
+
+//#include "asio/detail/bind_handler.hpp"
+#include "asio/placeholders.hpp"
+
+#include <memory>
+#include <thread>
+
+
 
 // io_service: http://think-async.com/Asio/asio-1.10.6/doc/asio/reference/io_service.html
 // timers: http://think-async.com/Asio/asio-1.10.6/doc/asio/overview/timers.html
+// http://en.cppreference.com/w/cpp/chrono
+// http://blog.think-async.com/2010/04/bind-illustrated.html
+
+void handler(int *x, const asio::error_code &ec)
+{
+	std::cout << "handler called" << std::endl;
+}
+
+void handler2(const asio::error_code &ec)
+{
+	std::cout << "handler 2 called" << std::endl;
+}
 
 int main()
 {
 	asio::io_service i;
+	// asio::io_service::work work(i);
+
 	asio::system_timer t(i);
 
-	t.expires_from_now(std::chrono::milliseconds(5000));
+	//t.expires_from_now(std::chrono::milliseconds(5000));
 
-	t.wait();
+	t.expires_at(std::chrono::system_clock::now() + std::chrono::milliseconds(3000));
 
-	asio::error_code ec;
+	int x;
 
-	i.run(ec);
+	// t.async_wait(handler2);
+	t.async_wait(std::bind(handler, &x, std::placeholders::_1));
 
-	std::cout << "error code: " << ec.value() << std::endl;
+	bool flag = false;
+
+	i.run();
+
 
 	return 0;
 }
